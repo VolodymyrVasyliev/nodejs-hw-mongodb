ea6fce4a534718736e1fcc056bc7b0eedd2881d8
Volodymyr Vasyl'ev <vasyliev.official@gmail.com>
2025-09-13 16:07:28 +0300

    add mangodb

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { getEnvVar } from '../utils/getEnvVar.js';
+
+export const initMongoConnection = async () => {
+  try {
+    const user = getEnvVar('MONGODB_USER');
+    const pwd = getEnvVar('MONGODB_PASSWORD');
+    const url = getEnvVar('MONGODB_URL');
+    const db = getEnvVar('MONGODB_DB');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}`,
+    );
+
+    console.log('✅ Mongo connection successfully established!');
+  } catch (error) {
+    console.error('❌ Error:', error.message);
+    throw error;
+  }
+};
+
--- src/server.js
+++ src/server.js
@@ -1,8 +1,76 @@
 import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
 
 export function setupServer() {
   const app = express();
-  const PORT = 3000;
+
+  app.use((req, res, next) => {
+    console.log(`Time: ${new Date().toLocaleString()}`);
+    next();
+  });
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: `Hello World!`,
+      Time: `${new Date().toLocaleString()}`,
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res
+      .status(200)
+      .json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+    res.status(200).json({
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use('/', (req, res, next) => {
+    res.status(404).json({
+      status: 404,
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
 
   app.listen(PORT, (error) => {
     if (error) {
