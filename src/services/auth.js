b2bd547fdbf9a336c50fdd7168269bd6c735f701
Volodymyr Vasyl'ev <vasyliev.official@gmail.com>
2025-10-03 22:20:34 +0300

    .

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,16 +1,40 @@
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { UserCollection } from '../db/models/User.js';
-
+import { SessionCollection } from '../db/models/session.js';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
+import { randomBytes } from 'crypto';
 
 export const registerUser = async (payload) => {
   const user = await UserCollection.findOne({ email: payload.email });
   if (user) throw createHttpError(409, 'Email in use');
-  
+
   const encryptedPassword = await bcrypt.hash(payload.password, 10);
-  
   return await UserCollection.create({
     ...payload,
     password: encryptedPassword,
+  });
+};
+
+export const loginUser = async (payload) => {
+  const user = await SessionCollection.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(401, 'User not found');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+  await SessionCollection.deleteOne({ userId: user._id });
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await SessionCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
 };
