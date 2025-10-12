5c56147819d801170974e6aa530f69b63dcbb0bd
Volodymyr Vasyl'ev <vasyliev.official@gmail.com>
2025-10-12 10:55:10 +0300

    reset-pwd

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -15,3 +15,7 @@ export const sendResetEmailSchema = Joi.object({
   email: Joi.string().email().required(),
 });
 
+export const resetPasswordSchema = Joi.object({
+  token: Joi.string().required(),
+  password: Joi.string().required(),
+});
