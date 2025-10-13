8b9709ef0e54efeaebe1c8f64e9e167196ed77c8
Volodymyr Vasyl'ev <vasyliev.official@gmail.com>
2025-10-13 11:20:21 +0300

    check phone number format

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,9 +1,15 @@
 import Joi from 'joi';
 
-
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number().integer().required(),
+  phoneNumber: Joi.string()
+    .pattern(/^\+380\d{9}$/) // Ukraine phone number format
+    .required()
+    .messages({
+      'string.pattern.base':
+        'The phone number must be in the format +380XXXXXXXXX',
+    })
+    .required(),
   email: Joi.string().email().lowercase().trim().required(),
   isFavourite: Joi.boolean().required(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
@@ -11,9 +17,8 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.number().integer(),
+  phoneNumber: Joi.number(),
   email: Joi.string().email().trim().lowercase(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
-
